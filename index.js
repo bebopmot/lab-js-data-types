7c396bed4aef89ac6805427c2ddab4dcd5971df6
bebopmot <bebopmotinc@gmail.com>
2025-04-02 15:46:11 +0200

    lab-js-data-types solution

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const sAll = s1+s2+s3+s4+s5
+console.log(sAll)
 
 // Print out the concatenated string
 
@@ -23,6 +24,13 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const modifiedPart1 = part1.slice(0, 3) + part1.slice(3).toUpperCase();
+
+const modifiedPart2 = part2.slice(0, 5) + part2.slice(5).toUpperCase();
+
+const result = modifiedPart1 + modifiedPart2;
+
+console.log(result);
 
 // Print the cameLtaiL-formatted string
 
@@ -35,10 +43,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tip = billTotal * 0.15
 
 // Print out the tipAmount
-
+console.log(tip)
 
 
 
@@ -47,10 +55,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
-
+const randoNum = Math.random() *10
+const randoNum2 = randoNum.toFixed(0)
 // Print the generated random number
-
+console.log(randoNum2)
 
 
 /*******************************************
@@ -61,16 +69,23 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; //false
+console.log(expression1)
 
-const expression2 = a || b;
+const expression2 = a || b; //true
+console.log(expression2)
 
-const expression3 = !a && b;
+const expression3 = !a && b; //true
+console.log(expression3)
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); //true
+console.log(expression4)
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; //true
+console.log(expression5)
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); //false
+console.log(expression6)
 
-const expression7 = a && a;
+const expression7 = a && a; //true
+console.log(expression7)
